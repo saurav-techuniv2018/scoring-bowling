7bc01da09d1c76897d27ad19eeb80bcead7df3ee
Saurav Sahu <sauravsahu.12@live.com>
2018-01-23 10:52:53 +0530

    verifyInput - tests passed when input contains invalid items and when numbers are out of range.
    verifyInput - tests passed when input contains invalid items and when numbers are out of range.

--- verify-input.js
+++ verify-input.js
@@ -1,5 +1,11 @@
+const verifyNumber = require('./verify-number');
+
 function verifyInput(scoreArray) {
   if (!Array.isArray(scoreArray)) { return false; }
+
+  for (let i = 0; i < scoreArray.length; i += 1) {
+    if (!verifyNumber(scoreArray[i])) return false;
+  }
   return true;
 }
 
--- verify-input.test.js
+++ verify-input.test.js
@@ -11,3 +11,11 @@ test('Should return true when input is undefined', () => {
 test('Should return false when input passed is not an array', () => {
   expect(verifyInput('scores')).toBe(false);
 });
+
+test('Should return false when array contains invalid items (Infinity, null and undefined)', () => {
+  expect(verifyInput([1, 3, null, undefined])).toBe(false);
+});
+
+test('Should return false when array contains scores that are out of range', () => {
+  expect(verifyInput([1, 5, 7, 9, -3, 12])).toBe(false);
+});
