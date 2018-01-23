4c6a24687e0340411d9cfeec4dc9e3ba95eee335
Saurav Sahu <sauravsahu.12@live.com>
2018-01-23 10:35:39 +0530

    verifyInput - Test passed for undefined and input not being an array.
    verifyInput - Test passed for undefined and input not being an array.

--- verify-input.test.js
+++ verify-input.test.js
@@ -3,3 +3,11 @@ const verifyInput = require('./verify-input');
 test('Should return false when input passed is null', () => {
   expect(verifyInput(null)).toBe(false);
 });
+
+test('Should return true when input is undefined', () => {
+  expect(verifyInput(undefined)).toBe(false);
+});
+
+test('Should return false when input passed is not an array', () => {
+  expect(verifyInput('scores')).toBe(false);
+});
