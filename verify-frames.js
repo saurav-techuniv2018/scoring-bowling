bc0bda1a9e962cd71dcec783fa5d429d228e5bee
Saurav Sahu <sauravsahu.12@live.com>
2018-01-25 13:00:26 +0530

    verifyFrames - add test cases for last round conditions and sum more than 10
    verifyFrames - add test cases for last round conditions and sum more than 10

--- verify-frames.js
+++ verify-frames.js
@@ -1,29 +1,83 @@
-function verifyFrames(scoresArray) {
-  // scoresArray has already been verified in the main score function
-  // not verifying again here
-  const resultFramesArray = scoresArray.reduce((accumulator, currentValue) => {
-    const framesArray = accumulator;
-    const frameNumber = framesArray.length - 1;
+const sumArray = require('./sum-array');
+const verifyNumber = require('./verify-number');
 
-    if (framesArray.length === 0) framesArray.push(currentValue);
+function verifyFrames(scoresArray, frames = 10) {
+  // Copy scoresArray to temporary array
+  let scores = scoresArray.slice();
 
-    else {
-      const currentFrame = framesArray[frameNumber];
-      if (currentFrame + currentValue <= 10) {
-        framesArray[frameNumber] += currentValue;
+  // A nested array that will hold the frames for the fame
+  const framesArray = [];
+  // Will hold the number of frames in the game
+  let framesCount = 0;
+  // Will hold temporarily, if the frames are complete
+  let isFrameComplete = true;
+  // Will hold the number of scores being added to framesArray
+  let addedScoresCount = 0;
+
+  const isValidFrames = scores.every((currentValue) => {
+    // Return false if score is invalid or if frames have been
+    // separated out till last round
+    if (!verifyNumber(currentValue)) { return false; }
+
+    if (framesCount === frames - 1) {
+      const lastRound = framesArray[frames - 1] || (framesArray[frames - 1] = []);
+
+      if (sumArray(lastRound) < 20 && lastRound.length < 2) {
+        lastRound.push(currentValue);
+        addedScoresCount += 1;
+        return true;
+      }
+      return true;
+    } else if (!isFrameComplete) {
+      // If frame is not complete, currentValue cannot be 10
+      if (currentValue === 10) return false;
+
+      framesArray[framesCount].push(currentValue);
+      addedScoresCount += 1;
+      // Complete frame
+      framesCount += 1;
+      isFrameComplete = true;
+    } else {
+      // Starting a new frame
+      framesArray[framesCount] = [];
+      framesArray[framesCount].push(currentValue);
+      addedScoresCount += 1;
+      if (currentValue === 10) {
+        // This frame is complete
+        framesCount += 1;
       } else {
-        framesArray.push(currentValue);
+        isFrameComplete = false;
       }
     }
+    return true;
+  });
 
-    return framesArray;
-  }, []);
+  if (!isValidFrames ||
+    framesArray.length < frames ||
+    framesArray.length > frames) { return false; }
 
-  if (resultFramesArray.length > 11 || resultFramesArray.length < 10) {
-    return false;
-  } else if (resultFramesArray[9] > 20) {
+
+  // Get the last round's scores
+  const lastRound = framesArray[frames - 1];
+
+  if (!(lastRound[0] + lastRound[1] <= 10 ||
+    lastRound[1] + lastRound[0] === 20)) {
     return false;
   }
+  // Remove all scores till the fill ball
+  scores = scores.splice(addedScoresCount);
+  const fillBall = lastRound[0] + lastRound[1] === 10 || lastRound[1] + lastRound[0] === 20;
+
+  if (!fillBall) {
+    return true;
+  }
+
+  // Add fill ball
+  if (!verifyNumber(scores[0])) {
+    return false;
+  }
+  lastRound.push(scores[0]);
+
   return true;
 }
 
--- verify-frames.test.js
+++ verify-frames.test.js
@@ -5,6 +5,15 @@ describe('verifyFrames', () => {
     test('when input scores does not make up all 10 frames', () => {
       expect(verifyFrames([1, 4, 5, 4])).toBe(false);
     });
+    test('when input contains round where a frame has sum more than 10', () => {
+      expect(verifyFrames([1, 5, 7, 10, 3, 6, 7, 2, 4, 5, 3, 4, 5, 2, 5, 3, 5, 2])).toBe(false);
+    });
+    test('when input contains last round with no fill ball when there is a with spare', () => {
+      expect(verifyFrames([1, 5, 1, 7, 3, 6, 7, 2, 4, 5, 3, 4, 5, 2, 5, 3, 2, 4, 5, 5])).toBe(false);
+    });
+    test('when input contains last round such that sum of first two throws is more than 10', () => {
+      expect(verifyFrames([1, 5, 1, 7, 3, 6, 7, 2, 4, 5, 3, 4, 5, 2, 5, 3, 3, 2, 5, 6, 2])).toBe(false);
+    });
   });
 
   describe('should return true', () => {
@@ -12,8 +21,8 @@ describe('verifyFrames', () => {
       expect(verifyFrames([3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6]))
         .toEqual(true);
     });
-    test('when input scores does indeed make up 10 frames', () => {
-      expect(verifyFrames([10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10]))
+    test('when input scores does indeed make up 10 frames with all strikes', () => {
+      expect(verifyFrames([10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10]))
         .toEqual(true);
     });
   });
