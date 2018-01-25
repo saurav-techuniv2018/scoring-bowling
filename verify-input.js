2a836cdea2c2aa15603ceb31eb5919cbbe86e624
Saurav Sahu <sauravsahu.12@live.com>
2018-01-25 13:15:54 +0530

    verifyInput - remove unnecessary function.
    verifyInput - remove unnecessary function.

--- verify-input.js
+++
@@ -1,9 +0,0 @@
-const verifyFrames = require('./verify-frames');
-
-function verifyInput(scoreArray) {
-  if (!Array.isArray(scoreArray)) { return false; }
-
-  return verifyFrames(scoreArray);
-}
-
-module.exports = verifyInput;
