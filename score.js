19b064fb5a1bead9baf85e212950671de2c4a776
Saurav Sahu <sauravsahu.12@live.com>
2018-01-25 13:13:19 +0530

    score - remove call to verifyInput (as all verification is done by verifyFrames)
    score - remove call to verifyInput (as all verification is done by verifyFrames)

--- score.js
+++ score.js
@@ -1,11 +1,7 @@
 const sumArray = require('./sum-array');
-const verifyInput = require('./verify-input');
 const verifyFrames = require('./verify-frames');
 
 function score(scoresArray) {
-  const validInput = verifyInput(scoresArray);
-  if (!validInput) return undefined;
-
   const validFrames = verifyFrames(scoresArray);
   if (!validFrames) return undefined;
 
