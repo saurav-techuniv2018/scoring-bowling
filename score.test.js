7dc9fa6a36c4b09c0250e72ebf12e3f3eb069f5d
Saurav Sahu <sauravsahu.12@live.com>
2018-01-26 09:53:26 +0530

    score - add extra test cases - all passed
    score - add extra test cases - all passed

--- score.test.js
+++ score.test.js
@@ -8,23 +8,26 @@ describe('score', () => {
     test('when input scores is empty', () => {
       expect(score([])).toBe(undefined);
     });
+    test('when input contains invalid numbers in the scores', () => {
+      expect(score([5, 3, 4, Infinity])).toBe(undefined);
+    });
   });
 
   describe('should return score', () => {
     test('when input scores contains no strikes or spares', () => {
       expect(score([3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6])).toBe(90);
     });
-
     test('when input scores contains some strikes but no spares', () => {
       expect(score([3, 6, 10, 7, 2, 5, 2, 2, 5, 4, 5, 7, 1, 10, 10, 4, 5])).toBe(120);
     });
-
-    test('when input scores contains some strikes and spares', () => {
-      expect(score([3, 6, 10, 7, 2, 5, 5, 2, 5, 4, 5, 7, 1, 10, 10, 4, 5])).toBe(125);
-    });
-
     test('when input contains only strikes', () => {
       expect(score([10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10])).toBe(300);
+    });
+    test('when input contains only rolls of 5 (all spares)', () => {
+      expect(score([5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5])).toBe(150);
+    });
+    test('when input scores contains some strikes and spares', () => {
+      expect(score([3, 6, 10, 7, 2, 5, 5, 2, 5, 4, 5, 7, 1, 10, 10, 4, 5])).toBe(125);
     });
   });
 });
