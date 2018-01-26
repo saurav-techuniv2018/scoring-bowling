c87da07f6aa0bb15d0c1b001ed700443c794cce0
Saurav Sahu <sauravsahu.12@live.com>
2018-01-26 18:45:22 +0530

    index - print usage when arguments are missing.
    index - print usage when arguments are missing.

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 const args = require('command-line-args');
+const printUsage = require('./print-usage');
 const score = require('./score');
 
 const optionDefinitions = [
   {
-    name: 'scores', alias: 's', type: Number, multiple: true, defaultOption: true,
+    name: 'scores',
+    alias: 's',
+    type: Number,
+    multiple: true,
+    defaultOption: true,
   },
   {
-    name: 'frames', alias: 'f', type: Number,
+    name: 'frames',
+    alias: 'f',
+    type: Number,
   },
 ];
 const receivedArguments = args(optionDefinitions);
+
+if (receivedArguments.scores === undefined) {
+  printUsage();
+  process.exit();
+}
 
 console.log(score(receivedArguments.scores, receivedArguments.frames));
---
+++ print-usage.js
@@ -0,0 +1,5 @@
+const printUsage = () => {
+  console.log('Provide arguments: [--frames | -f <Number of frames in the game>] [--scores | -s] <scores>');
+};
+
+module.exports = printUsage;
